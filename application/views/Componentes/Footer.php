b9b21f019eda9b8a51e9717004cdf24b30d261a7
TESMAN117 <67400857+TESMAN117@users.noreply.github.com>
2021-07-04 01:21:46 -0500

    23
    23

--- application/views/Componentes/Footer.php
+++ application/views/Componentes/Footer.php
@@ -26,7 +26,7 @@
     </script>
 
 </div>
-
+<div class="container"><h1>cambioss</h1></div>
 <script src="<?php echo ruta; ?>/assets/Librerias/Boostrap/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="<?php echo ruta; ?>/assets/Librerias/Boostrap/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="<?php echo ruta; ?>/assets/Librerias/Boostrap/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
