6f17f9438f905a98f58f1fdef74caf59df938c5e
BrisaMc <62993911+BrisaMc@users.noreply.github.com>
2021-07-07 01:03:54 -0500

    Update miperfil.php

--- application/views/Visitas/Socios/miperfil.php
+++ application/views/Visitas/Socios/miperfil.php
@@ -3,13 +3,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi perfil</title>
+    <title>Perfil</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    <center>
-    <h1>Estamos en mi perfil</h1>
-    </center>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Ver Perfil</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Nombre</label>
+                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Dirección</label>
+                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Correo</label>
+                        <p class="form-control-static"><?php echo $row["salary"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Volver</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
